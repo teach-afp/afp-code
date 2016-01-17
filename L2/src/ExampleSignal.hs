ae07ef0056d48317b1b9133059b6afc2b75167e7
Alejandro Russo <russo@chalmers.se>
2016-01-17 21:46:23 +0100

    Another example

--- L2/src/ExampleSignal.hs
+++ L2/src/ExampleSignal.hs
@@ -1,14 +1,14 @@
 -- | Examples for simple shapes.
 module ExampleSignal where
 
-import Signal  (Time, Signal, constS, timeS, ($$))
+import Signal  (Time, Signal, constS, timeS, ($$), mapT)
 import Shape   (Shape, disc, square)
 import Animate (animate)
 import Render  (defaultWindow)
 
 animateShape :: Signal Shape -> IO ()
 animateShape sh = animate defaultWindow 0 endTime sh
-  where endTime = 15
+  where endTime = 5
 
 -- | It displays a shape on "odd times" and another one in "even times".
 change :: Shape -> Shape -> Time -> Shape
@@ -18,3 +18,9 @@ change sh1 sh2 t
 
 square_disc :: Signal Shape
 square_disc = constS (change square disc) $$ timeS
+
+
+to_zero :: Time -> Time
+to_zero = const 0
+
+always_disc = mapT to_zero square_disc
