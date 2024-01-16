f4034b2debe11e6fe05cf8f9c856fd6a0c5312c7
Andreas Abel <andreas.abel@ifi.lmu.de>
2024-01-16 11:52:12 +0100

    [ ass2 ] testscotty: add logger

--- assignment2/replay/testscotty/TestScotty.hs
+++ assignment2/replay/testscotty/TestScotty.hs
@@ -1,33 +1,40 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
 import Control.Monad.Catch (SomeException, catch)
 import Data.Text.Lazy      (Text)
-import Web.Scotty          (ActionM, scotty, get, post, html, formParam)
+
+import Web.Scotty
+  ( ActionM, scotty, middleware, get, post, html, formParam )
+
+import Network.Wai.Middleware.RequestLogger
+  ( mkRequestLogger, defaultRequestLoggerSettings )
 
 main :: IO ()
-main = scotty 3000 $ do
-    get "/" serve
-    post "/" serve
+main = do
+  logger <- mkRequestLogger defaultRequestLoggerSettings
+  scotty 3000 do
+    middleware logger
+    get  "/" serve  -- Initial entrypoint
+    post "/" serve  -- Entry point for form submission with method=post
   where
     serve :: ActionM ()
     serve = do
-        i <- getInput
-        html (page i)
+      i <- getInput
+      html (page i)
 
     getInput :: ActionM Text
     getInput = formParam "text_input_id" `catch` \ (_ :: SomeException) -> return ""
 
     page :: Text -> Text
     page s = mconcat $
-        [ "<html><body>"
-        , "<p>Input was: ", s, "</p>"
-        , "<form method=post>"
-        , "<p>Type something here:</p>"
-        , "<input name=text_input_id>"
-        , "<input type=submit value=OK>"
-        , "</form>"
-        , "</body></html>"
-        ]
+      [ "<html><body>"
+      , "<p>Input was: ", s, "</p>"
+      , "<form method=post>"
+      , "<p>Type something here:</p>"
+      , "<input name=text_input_id>"
+      , "<input type=submit value=OK>"
+      , "</form>"
+      , "</body></html>"
+      ]
