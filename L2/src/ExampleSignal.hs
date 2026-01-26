4dafad372662ed488432b5570c9c6e04357c05b4
Andreas Abel <andreas.abel@ifi.lmu.de>
2026-01-26 11:24:11 +0100

    [L2] rename `change` to `alternate`

--- L2/src/ExampleSignal.hs
+++ L2/src/ExampleSignal.hs
@@ -11,13 +11,13 @@ animateShape sh = animate defaultWindow 0 endTime sh
   where endTime = 5
 
 -- | It displays a shape on "odd times" and another one in "even times".
-change :: Shape -> Shape -> Time -> Shape
-change sh1 sh2 t
-       | odd (floor t) = sh1
-       | otherwise     = sh2
+alternate :: Shape -> Shape -> Time -> Shape
+alternate sh1 sh2 t
+  | odd (floor t) = sh1
+  | otherwise     = sh2
 
 square_disc :: Signal Shape
-square_disc = signal (change disc square)
+square_disc = signal (alternate disc square)
 
 
 to_zero :: Time -> Time
